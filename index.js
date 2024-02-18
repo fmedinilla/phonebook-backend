0c6ecb8c4162693b7e4297444c6e92f34bc6aa8c
Francisco Medinilla Garcia <medinillagarciafrancisco@gmail.com>
2024-02-18 19:26:33 +0100

    exercise 3.1

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("Hello world");
+const express = require("express");
+
+const PORT = process.env.PORT ?? 3001;
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+const app = express();
+
+app.get("/api/persons", (request, response) => {
+  response.status(200).json(persons);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server on http://localhost:${PORT}`);
+});
